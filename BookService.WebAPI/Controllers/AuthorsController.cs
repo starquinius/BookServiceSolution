32483da3abec16aaca0c2cd30cc0d0b42593b80a
starquinius <fantasy@zeelandnet.nl>
2018-10-18 15:40:55 +0200

    Added AuthorsController

---
+++ BookService.WebAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookService.WebAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : Controller
+    {
+        BookRepository _authorRepository;
+
+        public AuthorsController(BookRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        //api/Authors
+        [HttpGet] //Can only be called with an get request. Ok is needed and gives status of request and objedct
+        public IActionResult GetAuthors()
+        {
+            return Ok(_authorRepository.List());
+        }
+
+        //api/Authors/Basic
+        [HttpGet]
+        [Route("Basic")] //Als woordje basic bij de route staat, dan deze httpget uitvoeren
+        public IActionResult GetAuthorBasic()
+        {
+            return Ok(_authorRepository.ListBasic());
+        }
+
+        //api/Authors/{id}
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetAuthor(int Id)
+        {
+            return Ok(_authorRepository.GetById(Id));
+        }
+
+    }
+}
