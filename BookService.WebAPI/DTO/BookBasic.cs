76ead9e23100d7e48866eed7c4b455cde25fb4d7
starquinius <fantasy@zeelandnet.nl>
2018-10-18 13:23:53 +0200

    Add Class BookBasicDTO

---
+++ BookService.WebAPI/DTO/BookBasic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.DTO
+{
+    public class BookBasic
+    { 
+        public int Id { get; set; }
+        public string Titel { get; set; }
+    }
+}
