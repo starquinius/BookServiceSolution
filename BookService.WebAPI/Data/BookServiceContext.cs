e21ae377e7db5b4c08ee703222ea6efd962ceefd
starquinius <fantasy@zeelandnet.nl>
2018-10-18 11:19:29 +0200

    Added DataSeeding (BookServiceContext)

--- BookService.WebAPI/Data/BookServiceContext.cs
+++ BookService.WebAPI/Data/BookServiceContext.cs
@@ -20,19 +20,31 @@ namespace BookService.WebAPI.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Publisher>()
+                .ToTable("Publisher")
                 .HasData(
                 new Publisher(1, "IT-Publishers", "UK"),
                 new Publisher(2, "FoodBooks", "Sweden")
                 );
 
             modelBuilder.Entity<Author>()
+                .ToTable("Author")
                 .HasData(
                 new Author(1,"James", "Sharp", new DateTime(1980, 5, 20)),
                 new Author(2, "Sophie", "Netty", new DateTime(1992,3,4)),
                 new Author(3, "Elisa", "Yammy", new DateTime(1996,8,12))
                 );
 
-
+            modelBuilder.Entity<Book>()
+                .ToTable("Book")
+                .HasData(//Anoniem
+                new { Id = 1, ISBN = "123456789", Title="Learning C#", NumberOfPages = 420, Filename = "book1.jpg", AuthorId=1, PublisherId=1},
+                new { Id = 2, ISBN = "45689132", Title = "Mastering Linq", NumberOfPages = 360, FileName = "book2.jpg", AuthorId = 2, PublisherId = 1 }, 
+                new { Id = 3, ISBN = "15856135", Title = "Mastering Xamarin", NumberOfPages = 360, FileName = "book3.jpg", AuthorId = 1, PublisherId = 1 }, 
+                new { Id = 4, ISBN = "56789564", Title = "Exploring ASP.Net Core 2.0", NumberOfPages = 360, FileName = "book1.jpg", AuthorId = 2, PublisherId = 1},
+                new { Id = 5, ISBN = "234546684", Title = "Unity Game Development", NumberOfPages = 420, FileName = "book2.jpg", AuthorId = 2, PublisherId = 1 }, 
+                new { Id = 6, ISBN = "789456258", Title = "Cooking is fun", NumberOfPages = 40, FileName = "book3.jpg", AuthorId = 3, PublisherId = 2 }, 
+                new { Id = 7, ISBN = "94521546", Title = "Secret recipes", NumberOfPages = 53, FileName = "book3.jpg", AuthorId = 3, PublisherId = 2 }
+                );
 
             base.OnModelCreating(modelBuilder);
         }
