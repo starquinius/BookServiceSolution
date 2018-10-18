dd5801e68a9144ea299c8339f125981d584b4a59
starquinius <fantasy@zeelandnet.nl>
2018-10-18 09:44:15 +0200

    Added Classes : Book, Author and Publisher

---
+++ BookService.WebAPI/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }        
+    }
+}
---
+++ BookService.WebAPI/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        [Display(Name="#")]
+        public int NumberOfPages { get; set; }
+        public Author Author { get; set; }
+        public Publisher Publisher { get; set; }
+        public string FileName { get; set; }
+        
+    }
+}
