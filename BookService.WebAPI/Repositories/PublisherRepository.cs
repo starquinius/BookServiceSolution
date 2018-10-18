8ac927235e7baf2b60f6ffc0dc06768992b587b1
starquinius <fantasy@zeelandnet.nl>
2018-10-18 14:22:32 +0200

    Added PublisherRepository

---
+++ BookService.WebAPI/Repositories/PublisherRepository.cs
@@ -0,0 +1,32 @@
+﻿using BookService.WebAPI.Data;
+using BookService.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.Repositories
+{
+    public class PublisherRepository
+    {
+        //Context
+        private BookServiceContext bookServiceContext;
+
+        public PublisherRepository(BookServiceContext context)
+        {
+            bookServiceContext = context;
+        }
+
+        //List all publishers
+        public List<Publisher> List()
+        {
+            return bookServiceContext.Publishers.ToList();
+        }
+
+        //List publisher by Id
+        public Publisher GetById(int publisherId)
+        {
+            return bookServiceContext.Publishers.Where(pu => pu.Id == publisherId).FirstOrDefault();
+        }
+    }
+}
