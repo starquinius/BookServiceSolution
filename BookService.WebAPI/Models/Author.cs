f4d3ff9b2741cd117e266d762d51cb13a5716812
starquinius <fantasy@zeelandnet.nl>
2018-10-18 10:57:41 +0200

    Updated Author with Constructor

--- BookService.WebAPI/Models/Author.cs
+++ BookService.WebAPI/Models/Author.cs
@@ -11,5 +11,15 @@ namespace BookService.WebAPI.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }        
+
+        public Author() { }
+
+        public Author(int id, string firstName, string lastName, DateTime birthDate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
     }
 }
