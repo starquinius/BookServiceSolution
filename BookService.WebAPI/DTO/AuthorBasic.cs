e92b62924067173aa8450c9089fe939a603037dc
starquinius <fantasy@zeelandnet.nl>
2018-10-18 14:13:37 +0200

    Added Classes BookDetail and AuthorBasic

---
+++ BookService.WebAPI/DTO/AuthorBasic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.DTO
+{
+    public class AuthorBasic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BookService.WebAPI/DTO/BookDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.WebAPI.DTO
+{
+    public class BookDetail
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public int NumberOfPages { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public int PublisherId { get; set; }
+        public string PublisherName { get; set; }
+        public string FileName { get; set; }
+    }
+}
